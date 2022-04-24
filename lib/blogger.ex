8fec77baa168eaba570bb51fb000c1a91b9273b0
Komenpurity <79756168+Komenpurity@users.noreply.github.com>
2022-04-24 22:36:28 +0300

    converting lower string into upper string

--- lib/blogger.ex
+++ lib/blogger.ex
@@ -12,7 +12,25 @@ defmodule Blogger do
       :world
 
   """
+   def hello do
+    :world
+  end
+
+
+  # fetching blog post from url
+
   def fetch_blog do
     HTTPoison.get! "https://medium.com/podiihq/quick-connect-to-your-amazon-ec2-linux-instance-through-the-command-line-6c682960ef91"
   end
+
+
+ # converting lower string inputs into uppercase inputs
+
+  def name(n) do
+        n
+        |> List.to_string
+        |> String.upcase 
+        |> String.split_at(3)  
+    end
+
 end
