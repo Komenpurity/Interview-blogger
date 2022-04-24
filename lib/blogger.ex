739862fb0b7fed31e98eba8484b2b657bb0c17bd
Komenpurity <79756168+Komenpurity@users.noreply.github.com>
2022-04-24 23:08:25 +0300

    fetching url and printing todo title

--- lib/blogger.ex
+++ lib/blogger.ex
@@ -17,11 +17,13 @@ defmodule Blogger do
   end
 
 
+
   # fetching blog post from url
 
   def fetch_blog do
     HTTPoison.get! "https://medium.com/podiihq/quick-connect-to-your-amazon-ec2-linux-instance-through-the-command-line-6c682960ef91"
   end
+
 
 
  # converting lower string inputs into uppercase inputs
@@ -31,6 +33,24 @@ defmodule Blogger do
         |> List.to_string
         |> String.upcase 
         |> String.split_at(3)  
+    end
+
+
+
+ # Fetching url using a library
+
+    def header do
+      HTTPoison.get! "https://jsonplaceholder.typicode.com/todos/1"
+
+      case HTTPoison.get("https://jsonplaceholder.typicode.com/todos/1") do
+        {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+          IO.puts body
+        {:ok, %HTTPoison.Response{status_code: 404}} ->
+          IO.puts "Not found :("
+        {:error, %HTTPoison.Error{reason: reason}} ->
+          IO.inspect reason
+      end
+
     end
 
 end
